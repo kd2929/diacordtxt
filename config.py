e27653cee282b625aeb7baf7bff6fc4a81461e55
Legendssssok <143305583+Legendssssok@users.noreply.github.com>
2024-01-19 15:35:03 +0000

    pylint: auto fixes

--- config.py
+++ config.py
@@ -1,9 +1,7 @@
 api_id = 3748059
 api_hash = "f8c9df448f3ba20a900bc2ffc8dae9d5"
 bot_token = "6934333025:AAEMtr5HKH6Vty8HDVkp31BEjJvh-Amwum4"
-auth_users = (
-    "5591734243, 1993514215, 5735651588, 870101201"
-)
+auth_users = "5591734243, 1993514215, 5735651588, 870101201"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "5735651588"
 owner_users = [int(num) for num in osowner_users.split(",")]
