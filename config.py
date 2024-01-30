3d105d81d7c24be0e87fe1b8f6d6fe3d18830946
adityasirji <146421388+adityasirji@users.noreply.github.com>
2024-01-30 07:07:45 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,7 +1,7 @@
-api_id = 3748059
-api_hash = "f8c9df448f3ba20a900bc2ffc8dae9d5"
-bot_token = "6934333025:AAEMtr5HKH6Vty8HDVkp31BEjJvh-Amwum4"
-auth_users = "5591734243, 1993514215, 5735651588, 870101201"
+api_id = 17864183
+api_hash = "b7cf3765dcf2e2781c98fc57f9f87a5b"
+bot_token = "6752335890:AAH0Dav1-QtqmsMLiaM2ho9MEcpuTRJSNdg"
+auth_users = "6629551092, 1318247204"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "5735651588"
+osowner_users = "6629551092"
 owner_users = [int(num) for num in osowner_users.split(",")]
