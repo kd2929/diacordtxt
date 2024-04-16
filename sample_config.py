4e2a341cac4e1563c1e10e5075cf544b475da38b
heynewbottrial <164299318+heynewbottrial@users.noreply.github.com>
2024-04-16 18:41:29 +0530

    Update sample_config.py

--- sample_config.py
+++ sample_config.py
@@ -1,9 +1,9 @@
 import os
 
-api_id = 3748059
-api_hash = os.environ.get("API_HASH", "f8c9df448f3ba20a900bc2ffc8dae9d5")
-bot_token = os.environ.get("BOT_TOKEN")
-auth_users = os.environ.get("AUTH_USERS", "5591734243,1369808729")
+api_id = 24262622
+api_hash = "50831eb3329ed9c0557aa2bc6aa34376"
+bot_token = "6981753441:AAGzoC0Tabw3Shp42fRy_TV4bOxvpl7Stpc"
+auth_users = "1477222048"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = os.environ.get("OWNER_USERS", "5591734243,1369808729")
+osowner_users = "6064206645"
 owner_users = [int(num) for num in osowner_users.split(",")]
