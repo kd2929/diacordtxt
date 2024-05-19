af8c7c7df41cc471e53c53df854fcafe89ee09e4
Lord <110597375+kd2929@users.noreply.github.com>
2024-05-19 20:36:50 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,5 +3,5 @@ api_hash = "cb3f118ce5553dc140127647edcf3720"
 bot_token = "5900432090:AAGh66GjvNR0fnGCiOngV4rC5tg5pninmlU"
 auth_users = "6175650047"
 sudo_users = [int(num) for num in auth_users.split(",")]
-osowner_users = "6175650047"
+osowner_users = "6175650047" "954672730"
 owner_users = [int(num) for num in osowner_users.split(",")]
