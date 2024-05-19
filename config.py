7cc9510942447a689072f6ff74b40dd9138a6509
Lord <110597375+kd2929@users.noreply.github.com>
2024-05-19 08:52:07 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 api_id = 24250238
 api_hash = "cb3f118ce5553dc140127647edcf3720"
-bot_token = "5827671272:AAEdqmWvjRtp8_Ry3gp6SOo2jcgCnPW8Ljw"
+bot_token = "6047785902:AAFmHAKkyofYcB6yS67nREtc5CKshZ1zhUE"
 auth_users = "6175650047"
 sudo_users = [int(num) for num in auth_users.split(",")]
 osowner_users = "6175650047"
